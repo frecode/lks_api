3b6e7a3bc5da50fa189206fd35da20c455ff2d77
rick <xjn@192.168.0.102>
2021-10-10 04:42:48 +0800

    V1.0.5 static

--- lks_api/urls.py
+++ lks_api/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from app01.views import init_web
+from django.views import static
+from lks_api.settings import STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app01.urls')),
     # path('init_web/', init_web),
+    re_path('^static/(?P<path>.*)$', static.serve, {'document_root': STATIC_ROOT}, name='static'),
 ]
